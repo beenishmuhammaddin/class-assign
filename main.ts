4984813e1ce1c7198ba57777dd26b2aebb7b32b0
BENISH RAJPUTSULEHRI <160973756+beenishmuhammaddin@users.noreply.github.com>
2024-03-12 09:33:58 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,95 @@
+//console.log("hello");
+
+//string type 
+
+//var message = "how are you";
+//console.log(message);
+//var message = "i am fine";
+//console.log(message);
+
+//number type:-
+//let b : number=30
+//console.log(b)\
+
+// boolean with example:-
+//let d:boolean = false
+//console.log(d);
+
+//null with example:-
+//let c:null = null
+//console.log(c);
+
+// camelCase
+//snake_case
+//pascalCase
+//var userName = 'Noman'; // e.g camelcase
+//var user_name = 'owais'; // e.g snake_case
+
+//undefined with example:-
+//let message = undefined
+//console.log(message
+
+//Symbol with example:-
+//let symbol = "$"
+//console.log(symbol);
+
+//bigint with example:-
+//let bigint = (-9);
+//console.log(bigint);
+
+//array with example:-
+//let dailyActivities = ['eat','read','sleep'];
+//console.log(dailyActivities);
+
+//object wit example:-
+//const student = {firstname:'hiba',class:10};
+//  console.log(student); .
+//camelCase
+//snake_case
+//pascalCase
+//let userName = 'Noman' // e.g camelcase
+//let user_name = 'owais' // e.g snake_case
+//let UserName = 'BinishRajput'//e.g pascalcase
+
+//let mydailyactivities:string[] =['eat','sleep','read'];
+//console.log(mydailyactivities);
+//let firstactivity = mydailyactivities[0]
+//console.log(firstactivity);
+//console.log(mydailyactivities[1],mydailyactivities[2]);
+
+let fruit: string[] = ["mango","apple"] // array
+
+//object =>{}=>{key:value} pair
+let user = {
+firstName: 'john',
+lastName: 'doe',
+age: '40',
+};
+let student2 = {
+fullName: "binish",
+classId: 12345,
+rollno: 3,
+Email: "bg@gmail.com",
+ispassed:true,
+subjects: ['urdu','math','computer']
+};
+let studentinfo = [user,student2,
+username: "binish",
+userage : 25,
+]
+/*[]
+[vlue,value2]=> array
+["a","b"]=> array of string
+[[],[],[]]=> array of array
+[[],{},[]]*/
+
+let arr = [{user},{student2},[fruits],[studentinfo]]
+let obj ={
+    key:{
+        key:{
+        a:'binish',
+        b:'Rajput',
+        }
+    },
+    key2: ["class","rollno"]
+}
